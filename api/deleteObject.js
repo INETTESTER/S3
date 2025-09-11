6968cb9b48a6cac943016f2eda99aa0e00e8eb3b
Tester Inet Doc <tester.inet.doc@gmail.com>
2025-09-11 13:54:36 +0700

    delete

--- api/deleteObject.js
+++ api/deleteObject.js
@@ -1,25 +1,40 @@
 import { AWSConfig, S3Client } from 'https://jslib.k6.io/aws/0.14.0/s3.js';
+import { AWS_ACCESS_KEY_ID, AWS_ACCESS_KEY_ID2, AWS_ENDPOINT_URL, AWS_ENDPOINT_URL2, AWS_REGION, AWS_REGION2, AWS_SECRET_ACCESS_KEY, AWS_SECRET_ACCESS_KEY2, S3_BUCKET_NAME } from './env.js';
 
 // กำหนด config S3
+// const awsConfig = new AWSConfig({
+//     region: "us-east-1",
+//     accessKeyId: "xfJm1oGpEdvNHVx0pNXr",
+//     secretAccessKey: "unEcVjXtmSpLUwDCobG5zKV1vLD4d6lmbUK7i8t3",
+//     endpoint: "https://inet-s3-object-gw-c.inet.co.th",
+//     forcePathStyle: true,
+// });
 const awsConfig = new AWSConfig({
-    region: "us-east-1",
-    accessKeyId: "xfJm1oGpEdvNHVx0pNXr",
-    secretAccessKey: "unEcVjXtmSpLUwDCobG5zKV1vLD4d6lmbUK7i8t3",
-    endpoint: "https://inet-s3-object-gw-c.inet.co.th",
+    region: AWS_REGION,
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY,
+    endpoint: AWS_ENDPOINT_URL,
     forcePathStyle: true,
 });
-
+// const awsConfig = new AWSConfig({
+//     region: AWS_REGION2,
+//     accessKeyId: AWS_ACCESS_KEY_ID2,
+//     secretAccessKey: AWS_SECRET_ACCESS_KEY2,
+//     endpoint: AWS_ENDPOINT_URL2,
+//     forcePathStyle: true,
+// });
 const s3 = new S3Client(awsConfig);
 
 
 // ---------- delete ----------
-export async function deleteObject() {
-    const objectKey = `simple-put-${__VU}-${__ITER}.bin`;
+export async function deleteObject(cid) {
+    const objectKey = `loadtest4/simple-put-${__VU}-${__ITER}${cid}.bin`;
     try {
         const res = await s3.deleteObject("test-zte", objectKey);
-        console.log(`🗑️ Deleted: ${objectKey}`);
+        //console.log(`🗑️ Deleted: ${objectKey}`);
         return res;
     } catch (err) {
         console.error(`DeleteObject error: ${err}`);
     }
 }
+
