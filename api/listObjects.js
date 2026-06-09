3a42a51be940082b2eccf455f427ccd9867afe55
Bancha <tester.inet.doc@gmail.com>
2026-06-09 17:10:21 +0700

    a

--- api/listObjects.js
+++ api/listObjects.js
@@ -1,5 +1,11 @@
 import { AWSConfig, S3Client } from 'https://jslib.k6.io/aws/0.14.0/s3.js';
-import { AWS_ACCESS_KEY_ID, AWS_ENDPOINT_URL, AWS_REGION, AWS_SECRET_ACCESS_KEY, S3_BUCKET_NAME } from './env.js';
+import {
+    AWS_ACCESS_KEY_ID,
+    AWS_ENDPOINT_URL,
+    AWS_REGION,
+    AWS_SECRET_ACCESS_KEY,
+    S3_BUCKET_NAME
+} from './env.js';
 
 const awsConfig = new AWSConfig({
     region: AWS_REGION,
@@ -8,15 +14,21 @@ const awsConfig = new AWSConfig({
     endpoint: AWS_ENDPOINT_URL,
     forcePathStyle: true,
 });
+
 const s3 = new S3Client(awsConfig);
 
 // ---------- function listObjects ----------
 export async function listObjects() {
     try {
         const res = await s3.listObjects(S3_BUCKET_NAME);
-        //console.log(`Objects in ${bucketName}: ${JSON.stringify(res)}`);
+
+        // console.log(
+        //     `Objects in ${S3_BUCKET_NAME}: ${JSON.stringify(res)}`
+        // );
+
         return res;
     } catch (err) {
         console.error(`ListObjects error: ${err}`);
+        return null;
     }
 }
